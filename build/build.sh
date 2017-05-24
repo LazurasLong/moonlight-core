84d08e1e2462146a8b37e27634de83ba9493fb20
Elsseriah Decieux <pokeplun@gmail.com>
2017-05-24 08:52:30 -0600

    embed css

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,11 @@
 mkdir -p '../text/html/'
 mkdir -p '../text/pdf/'
-cp 'resources/style.css' '../text/html/style.css'
 
-pandoc ../source/*.md -S --toc --template 'resources/template.html' -c 'style.css' -t html -s -o '../text/html/moonlight.html'
+pandoc ../source/*.md -T "Moonlight" -S --toc --template 'resources/template.html' -t html -s -o '../text/html/moonlight.html'
 pandoc ../source/*.md -V fontsize=12pt --toc -t latex -s -o "../text/pdf/moonlight.pdf"
 
 for file in ../source/*.md; do
     filename=$(basename "$file")
-    pandoc "$file" -S --toc --template 'resources/template.html' -c 'style.css' -t html -s -o "../text/html/${filename%.*}.html"
+    pandoc "$file" -S -T "${filename%.*}" --toc --template 'resources/template.html' -t html -s -o "../text/html/${filename%.*}.html"
     pandoc "$file" -V fontsize=12pt -t latex -s -o "../text/pdf/${filename%.*}.pdf"
 done
