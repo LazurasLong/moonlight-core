657245e6e7f58606a801e956b00c19260f15d256
Elsseriah Decieux <pokeplun@gmail.com>
2017-05-24 08:34:17 -0600

    streamline system

--- build/build.sh
+++ build/build.sh
@@ -2,11 +2,11 @@ mkdir -p '../text/html/'
 mkdir -p '../text/pdf/'
 cp 'resources/style.css' '../text/html/style.css'
 
-pandoc ../source/*.md -S --toc -f markdown --template 'resources/template.html' -c 'style.css' -t html -s -o '../text/html/moonlight.html'
-pandoc ../source/*.md -V geometry:margin=1.2in -t latex -s -o "../text/pdf/moonlight.pdf"
+pandoc ../source/*.md -S --toc --template 'resources/template.html' -c 'style.css' -t html -s -o '../text/html/moonlight.html'
+pandoc ../source/*.md -V fontsize=12pt --toc -t latex -s -o "../text/pdf/moonlight.pdf"
 
 for file in ../source/*.md; do
     filename=$(basename "$file")
-    pandoc "$file" -S --toc -f markdown --template 'resources/template.html' -c 'style.css' -t html -s -o "../text/html/${filename%.*}.html"
-    pandoc "$file" -V geometry:margin=1.2in -t latex -s -o "../text/pdf/${filename%.*}.pdf"
+    pandoc "$file" -S --toc --template 'resources/template.html' -c 'style.css' -t html -s -o "../text/html/${filename%.*}.html"
+    pandoc "$file" -V fontsize=12pt -t latex -s -o "../text/pdf/${filename%.*}.pdf"
 done
